263cda405eb3820f225ae9d4b83e64dd0239808b
Muhammad Hari Suharto <muharihar@infoterkini.com>
2018-10-16 17:07:21 +0700

    Add Module Data Response Type

--- kaiwsdkv2/response_types.go
+++ kaiwsdkv2/response_types.go
@@ -1,1 +1,22 @@
 package kaiwsdkv2
+
+// GetDestinationRS represent "data.get_des" response
+type GetDestinationRS struct {
+	ErrCode      interface{}     `json:"err_code"`
+	ErrMsg       interface{}     `json:"err_msg"`
+	Destinations [][]interface{} `json:"destination"`
+}
+
+// GetOriginationRS represent "data.get_org" response
+type GetOriginationRS struct {
+	ErrCode      interface{}     `json:"err_code"`
+	ErrMsg       interface{}     `json:"err_msg"`
+	Originations [][]interface{} `json:"origination"`
+}
+
+// GetPayTypeRS represent "data.get_pay_type" response
+type GetPayTypeRS struct {
+	ErrCode  interface{}   `json:"err_code"`
+	ErrMsg   interface{}   `json:"err_msg"`
+	PayTypes []interface{} `json:"pay_type"`
+}
